89c006e04024eb78226478e7e068b81655e6aa84
ay9thqi3tbqiwbegqsg a[soiaosshasdg <30902201+alx365@users.noreply.github.com>
2020-10-20 19:57:14 +0000

    added expanded boolean to the expander (#36)

--- src/widgets/widget_definitions.rs
+++ src/widgets/widget_definitions.rs
@@ -149,8 +149,10 @@ pub(super) fn resolve_orientable_attrs(bargs: &mut BuilderArgs, gtk_widget: &gtk
 fn build_gtk_expander(bargs: &mut BuilderArgs) -> Result<gtk::Expander> {
     let gtk_widget = gtk::Expander::new(None);
     resolve_block!(bargs, gtk_widget, {
-        // @prop label - label of the expander
-        prop(label: as_string) { gtk_widget.set_label(Some(&label)); }
+    // @prop name - name of the expander
+    prop(name: as_string) {gtk_widget.set_label(Some(&name));},
+    // @prop expanded - sets if the tree is expanded
+    prop(expanded: as_bool) { gtk_widget.set_expanded(expanded); }
     });
     Ok(gtk_widget)
 }
