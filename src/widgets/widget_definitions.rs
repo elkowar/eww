69158a19764279ef31e887138aec3fc3e7ec085a
ur name <legendofmiracles@protonmail.com>
2020-10-20 18:25:55 +0000

    added expanded boolean to the expander

--- src/widgets/widget_definitions.rs
+++ src/widgets/widget_definitions.rs
@@ -149,10 +149,14 @@ pub(super) fn resolve_orientable_attrs(bargs: &mut BuilderArgs, gtk_widget: &gtk
 
 /// @widget expander widget
 fn build_gtk_expander(bargs: &mut BuilderArgs) -> Result<gtk::Expander> {
-    let gtk_widget = gtk::Expander::new(None);
+    let gtk_widget = gtk::Expander::new(Some("Placeholder text, don't forget to set the property 'name'"));
     resolve_block!(bargs, gtk_widget, {
-        // @prop label - label of the expander
-        prop(label: as_string) { gtk_widget.set_label(Some(&label)); }
+    // @prop label - label of the expander
+    prop(label: as_string) {gtk_widget.set_label(Some(&label));},
+    // @prop expanded - sets if the tree is expanded
+    prop(expanded: as_bool) {
+        gtk_widget.set_expanded(expanded);
+    }
     });
     Ok(gtk_widget)
 }
