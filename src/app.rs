3bd73b46f2419e1fbc036411e1077cc68147e337
elkowar <5300871+elkowar@users.noreply.github.com>
2020-10-23 20:28:18 +0200

    Stop script-vars and close windows on eww kill

--- src/app.rs
+++ src/app.rs
@@ -61,6 +61,8 @@ impl App {
             EwwCommand::ReloadCss(css) => self.load_css(&css),
             EwwCommand::KillServer => {
                 log::info!("Received kill command, stopping server!");
+                self.script_var_handler.stop();
+                self.windows.values().for_each(|w| w.gtk_window.close());
                 std::process::exit(0);
             }
             EwwCommand::OpenWindow { window_name, pos, size } => self.open_window(&window_name, pos, size),
