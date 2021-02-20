d086cfa5fd8e54eb11904ec187edc8eb1c58326f
legendofmiracles <legendofmiracles@protonmail.com>
2021-02-20 20:55:21 +0000

    not going to implement disk IO. go back to this commit, to see a rough idea

--- src/config/custom_inbuilt/disk_io.rs
+++ src/config/custom_inbuilt/disk_io.rs
@@ -1,3 +1,4 @@
+// NOT GOING TO IMPLEMENT THIS!
 // why can't this stupid lib just support this reeeeeeee (https://github.com/GuillaumeGomez/sysinfo/issues/38)
 use anyhow::*;
 use lazy_static::lazy_static;
@@ -5,7 +6,7 @@ use std::{
     fs,
     sync::{Arc, Mutex},
 };
-use sysinfo::{DiskExt, RefreshKind, System, SystemExt};
+use sysinfo::{Disk, DiskExt, RefreshKind, System, SystemExt};
 
 lazy_static! {
     static ref LAST_VALUES: Arc<Mutex<(usize, usize)>> = Arc::new(Mutex::new((0, 0)));
@@ -20,27 +21,38 @@ fn get_current_values() -> Result<(usize, usize)> {
     let disks: Vec<&str> = disks
         .get_disks()
         .iter()
-        .map(|a| a.get_mount_point().file_name().unwrap().to_str().unwrap())
+        .map(|a: &Disk| {
+            std::path::Path::new(a.get_name().clone())
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+        })
         .collect();
     // https://www.kernel.org/doc/Documentation/iostats.txt
     let fields: Vec<&str> = file
         .lines()
+        .map(|a| a.trim())
         .filter(|a| disks.contains(&a.split_whitespace().nth(2).unwrap()))
         .collect();
-    let mut values: Vec<(usize, usize)> = Vec::new();
+    println!("{:#?}", fields);
+    let mut write: usize = 0;
+    let mut read: usize = 0;
     for i in fields {
-        let read = i.split_whitespace().nth(5).unwrap().parse()?;
-        let write = i.split_whitespace().nth(9).unwrap().parse()?;
-        values.push((read, write));
+        read = read + i.split_whitespace().nth(5).unwrap().parse::<usize>()?;
+        write = write + i.split_whitespace().nth(9).unwrap().parse::<usize>()?;
     }
-    Ok((1, 1))
+    Ok((write, read))
 }
 
 pub fn get_disk_write() -> Result<f32> {
     let current_values = get_current_values()?;
+    println!("current: {:#?}", current_values);
     let tmp = LAST_VALUES.clone();
+    println!("last: {:#?}", tmp);
     let mut last_values = tmp.lock().unwrap();
     let usage = current_values.clone().1 as f32 - last_values.1.clone() as f32;
     *last_values = current_values;
-    Ok(usage * 496)
+    println!("new last: {:#?}", last_values);
+    Ok(usage * 4096 as f32)
 }
