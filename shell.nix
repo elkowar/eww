57c674fb02faad05810bd22bca955daa77988e30
karetsu <20670199+karetsu@users.noreply.github.com>
2021-03-19 10:14:32 +0000

    replaced rustup with mozilla rust nightly

--- shell.nix
+++ shell.nix
@@ -1,8 +1,23 @@
 with import <nixpkgs> { };
 
-mkShell rec {
+let
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in mkShell rec {
   name = "eww";
 
-  buildInputs = [ glib rustup cairo atk gdk-pixbuf pango gtk3 ];
+  buildInputs = [
+    glib
+    cairo
+    atk
+    gdk-pixbuf
+    pango
+    gtk3
+    (nixpkgs.rustChannelOf {
+      date = "2021-03-16";
+      channel = "nightly";
+    }).rust
+  ];
   nativeBuildInputs = [ pkgconfig wrapGAppsHook ];
 }
