eb804ae3467200a7b0d162001fea02575805b2e7
elkowar <5300871+elkowar@users.noreply.github.com>
2021-03-23 11:14:06 +0100

    update dependencies

--- src/display_backend.rs
+++ src/display_backend.rs
@@ -80,7 +80,7 @@ mod platform {
 
             self.conn
                 .change_property(
-                    PropMode::Replace,
+                    PropMode::REPLACE,
                     win_id,
                     self.atoms._NET_WM_STRUT,
                     self.atoms.CARDINAL,
@@ -91,7 +91,7 @@ mod platform {
                 .check()?;
             self.conn
                 .change_property(
-                    PropMode::Replace,
+                    PropMode::REPLACE,
                     win_id,
                     self.atoms._NET_WM_STRUT_PARTIAL,
                     self.atoms.CARDINAL,
