95864e61b95649d28c55bcca6316a5e89afe6b58
Vertecedoc4545 <58826034+Vertecedoc4545@users.noreply.github.com>
2021-09-12 11:58:08 -0500

    Updated Shell.nix
    Bug corretion: error: infinite recursion encountered, at undefined position

--- shell.nix
+++ shell.nix
@@ -1,31 +1,32 @@
-{ pkgs ? import <nixpkgs> {
-    overlays = [
-      (import (fetchTarball
-      "https://github.com/nix-community/fenix/archive/main.tar.gz"))
-      (self: super: {
-          rustc = super.fenix.latest.rustc;
-          cargo  = super.fenix.latest.cargo;
-          rust-src = super.fenix.latest.rust-src;
-      }
-        )
-    ];
-  }
-}:
+{npkgs ? import <nixpkgs> {}}:
 
-pkgs.mkShell {
-  packages = with pkgs; [
-    rustc
-    cargo
-    rust-analyzer
-    gcc
-    gtk3
-    pkg-config
-    rustfmt-preview
-    clippy-preview
-    deno
-    mdbook
-  ];
+let
+    fenix = import
+      (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz")
+      { };
+in
+  let
+    rustc = fenix.latest.rustc;
+    cargo  = fenix.latest.cargo;
+    rust-src = fenix.latest.rust-src;
+    rustfmt-preview = fenix.latest.rustfmt-preview;
+    clippy-preview = fenix.latest.clippy-preview;
+  in
 
 
-  RUST_SRC_PATH = "${pkgs.rust-src}/lib/rustlib/src/rust/library";
-}
+    npkgs.mkShell {
+      packages = [
+          rustc
+          cargo
+          npkgs.rust-analyzer
+          npkgs.gcc
+          npkgs.gtk3
+          rust-src
+          npkgs.pkg-config
+          rustfmt-preview
+          clippy-preview
+          npkgs.deno
+          npkgs.mdbook
+        ];
+      RUST_SRC_PATH = "${rust-src}/lib/rustlib/src/rust/library";
+    }
