02fbf9b20d83a4efbe10a0ed576d289d0c1cddf8
elkowar <5300871+elkowar@users.noreply.github.com>
2021-10-27 17:19:03 +0200

    Scope graph implementation

--- crates/eww/src/pettree.rs
+++ crates/eww/src/pettree.rs
@@ -1,15 +1,36 @@
+use anyhow::{Context, Result};
+use eww_shared_util::VarName;
 use petgraph::{
     graph::{DiGraph, NodeIndex},
     EdgeDirection::{Incoming, Outgoing},
 };
 
-#[derive(Debug)]
-pub struct PetTree<N> {
-    graph: DiGraph<N, ()>,
-    root_index: NodeIndex,
+pub trait HasScopeContents {
+    fn has_variable(&self, var_name: &VarName) -> bool;
 }
 
-impl<N> PetTree<N> {
+#[derive(Debug, Eq, PartialEq)]
+enum ScopeTreeEdge {
+    ChildOf,
+    ReferencesVariable(VarName),
+}
+
+impl ScopeTreeEdge {
+    fn references_var(&self, name: &VarName) -> bool {
+        match self {
+            ScopeTreeEdge::ChildOf => false,
+            ScopeTreeEdge::ReferencesVariable(x) => x == name,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ScopeTree<N> {
+    graph: DiGraph<N, ScopeTreeEdge>,
+    pub root_index: NodeIndex,
+}
+
+impl<N: HasScopeContents> ScopeTree<N> {
     pub fn new(root: N) -> Self {
         let mut graph = DiGraph::new();
         let root_index = graph.add_node(root);
@@ -19,8 +40,24 @@ impl<N> PetTree<N> {
     /// Add a new child to the tree. panics if the given node does not exist in the tree.
     pub fn add_node(&mut self, child_of: NodeIndex, value: N) -> NodeIndex {
         let new_index = self.graph.add_node(value);
-        self.graph.add_edge(child_of, new_index, ());
+        self.graph.add_edge(child_of, new_index, ScopeTreeEdge::ChildOf);
         new_index
+    }
+
+    pub fn add_var_reference_to_node(&mut self, index: NodeIndex, var_name: VarName) -> Result<()> {
+        let node = self.value_at(index).context("Given index is not in the graph")?;
+        if !node.has_variable(&var_name) {
+            let mut cur_idx = index;
+            while let Some(parent) = self.parent_of(cur_idx) {
+                let node = self.value_at(index).expect("Nodes parent was not in the graph...");
+                if node.has_variable(&var_name) {
+                    self.graph.add_edge(parent, index, ScopeTreeEdge::ReferencesVariable(var_name));
+                    break;
+                }
+                cur_idx = parent;
+            }
+        }
+        Ok(())
     }
 
     pub fn remove_node_recursively(&mut self, index: NodeIndex) {
@@ -39,13 +76,47 @@ impl<N> PetTree<N> {
         self.graph.node_weight_mut(index)
     }
 
-    pub fn parent_of(&self, index: NodeIndex) -> Option<NodeIndex> {
-        let mut parents = self.graph.neighbors_directed(index, Incoming).detach();
-        let parent = parents.next_node(&self.graph);
-        // Given that there is no way for a node to _get_ more than one parents, we know this is fine.
-        assert!(parents.next_node(&self.graph).is_none());
+    pub fn children_referencing(&self, index: NodeIndex, var_name: &VarName) -> Vec<NodeIndex> {
+        self.neighbors_where(index, Outgoing, |edge| edge.references_var(var_name))
+    }
 
-        parent
+    fn find_neighbor(
+        &self,
+        index: NodeIndex,
+        dir: petgraph::EdgeDirection,
+        f: impl Fn(&ScopeTreeEdge) -> bool,
+    ) -> Option<NodeIndex> {
+        let mut neighbors = self.graph.neighbors_directed(index, dir).detach();
+        while let Some(neighbor) = neighbors.next_node(&self.graph) {
+            let edges = match dir {
+                Outgoing => self.graph.edges_connecting(index, neighbor),
+                Incoming => self.graph.edges_connecting(neighbor, index),
+            };
+            if edges.into_iter().any(|x| f(x.weight())) {
+                return Some(neighbor);
+            }
+        }
+        None
+    }
+
+    fn neighbors_where(
+        &self,
+        index: NodeIndex,
+        dir: petgraph::EdgeDirection,
+        f: impl Fn(&ScopeTreeEdge) -> bool,
+    ) -> Vec<NodeIndex> {
+        let mut neighbors = self.graph.neighbors_directed(index, dir).detach();
+        let mut result = Vec::new();
+        while let Some(neighbor) = neighbors.next_node(&self.graph) {
+            if self.graph.edges_connecting(index, neighbor).into_iter().any(|x| f(x.weight())) {
+                result.push(neighbor);
+            }
+        }
+        result
+    }
+
+    pub fn parent_of(&self, index: NodeIndex) -> Option<NodeIndex> {
+        self.find_neighbor(index, Incoming, |edge| edge == &ScopeTreeEdge::ChildOf)
     }
 
     /// Search through the ancestors of a node for a value that satisfies the given predicate.
