40abb6426ba4f444621f8f9f7aaa9d0665a9f3bf
elkowar <5300871+elkowar@users.noreply.github.com>
2021-11-05 16:30:13 +0100

    Allow for optional values in def_widget

--- crates/eww/src/widgets/def_widget_macro.rs
+++ crates/eww/src/widgets/def_widget_macro.rs
@@ -2,7 +2,9 @@
 macro_rules! def_widget {
     ($args:ident, $scope_graph:ident, $gtk_widget:ident, {
         $(
-            prop( $( $attr_name:ident : $typecast_func:ident $(= $default:expr)?),*) $code:block
+            prop($(
+                $attr_name:ident : $typecast_func:ident $(? $(@ $optional:tt @)?)? $(= $default:expr)?
+            ),*) $code:block
         ),+ $(,)?
     }) => {
         $({
@@ -10,16 +12,16 @@ macro_rules! def_widget {
                 $args.unhandled_attrs.retain(|a| &a.0 != &::std::stringify!($attr_name).replace('_', "-"));
             )*
 
-            let attr_map: Result<HashMap<eww_shared_util::AttrName, simplexpr::SimplExpr>> = try {
+            let attr_map: Result<HashMap<eww_shared_util::AttrName, Option<simplexpr::SimplExpr>>> = try {
                 ::maplit::hashmap! {
                     $(
                         eww_shared_util::AttrName(::std::stringify!($attr_name).to_owned()) =>
-                            def_widget!(@get_value $args, &::std::stringify!($attr_name).replace('_', "-"), $(= $default)?)
+                            def_widget!(@get_value $args, &::std::stringify!($attr_name).replace('_', "-"), $(? $($optional)?)? $(= $default)?)
                     ),*
                 }
             };
             if let Ok(attr_map) = attr_map {
-                let required_vars = attr_map.values().flat_map(|expr| expr.collect_var_refs()).collect();
+                let required_vars = attr_map.values().flat_map(|expr| expr.as_ref().map(|x| x.collect_var_refs()).unwrap_or_default()).collect();
                 $args.scope_graph.register_listener(
                     $args.calling_scope,
                         crate::state::scope::Listener {
@@ -32,9 +34,15 @@ macro_rules! def_widget {
                                 $(
                                     let attr_name: &str = ::std::stringify!($attr_name);
                                     let $attr_name = attr_map.get(attr_name)
-                                        .context("Missing attribute, this should never happen")?
-                                        .eval(&values)?
-                                        .$typecast_func()?;
+                                        .context("Missing attribute, this should never happen")?;
+
+                                    let $attr_name = if let Some(x) = $attr_name {
+                                        Some(x.eval(&values)?.$typecast_func()?)
+                                    } else {
+                                        None
+                                    };
+                                    def_widget!(@unwrap_if_required $attr_name $(? $($optional)?)?);
+
                                 )*
                                 $code
                                 Ok(())
@@ -46,13 +54,20 @@ macro_rules! def_widget {
         })+
     };
 
+    (@unwrap_if_required $value:ident ?) => { };
+    (@unwrap_if_required $value:ident) => {
+        let $value = $value.unwrap();
+    };
+
+    (@get_value $args:ident, $name:expr, ?) => {
+        $args.widget_use.attrs.ast_optional::<simplexpr::SimplExpr>($name)?.clone()
+    };
 
     (@get_value $args:ident, $name:expr, = $default:expr) => {
-        $args.widget_use.attrs.ast_optional::<simplexpr::SimplExpr>($name)?.clone().unwrap_or(simplexpr::SimplExpr::synth_literal($default))
+        Some($args.widget_use.attrs.ast_optional::<simplexpr::SimplExpr>($name)?.clone().unwrap_or(simplexpr::SimplExpr::synth_literal($default)))
     };
 
     (@get_value $args:ident, $name:expr,) => {
-        $args.widget_use.attrs.ast_required::<simplexpr::SimplExpr>($name)?.clone()
+        Some($args.widget_use.attrs.ast_required::<simplexpr::SimplExpr>($name)?.clone())
     }
 }
-
