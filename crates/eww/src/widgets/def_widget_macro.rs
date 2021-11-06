d7990b51c8c0da39805d69805e9edc03ebd8f07e
elkowar <5300871+elkowar@users.noreply.github.com>
2021-11-06 12:32:46 +0100

    Fix defmacro always calling all-optional props

--- crates/eww/src/widgets/def_widget_macro.rs
+++ crates/eww/src/widgets/def_widget_macro.rs
@@ -21,35 +21,41 @@ macro_rules! def_widget {
                 }
             };
             if let Ok(attr_map) = attr_map {
-                let required_vars = attr_map.values().flat_map(|expr| expr.as_ref().map(|x| x.collect_var_refs()).unwrap_or_default()).collect();
-                $args.scope_graph.register_listener(
-                    $args.calling_scope,
-                        crate::state::scope::Listener {
-                        needed_variables: required_vars,
-                        f: Box::new({
-                            let $gtk_widget = $gtk_widget.clone();
-                            move |$scope_graph, values| {
-                                // value is a map of all the variables that are required to evaluate the
-                                // attributes expression.
-                                $(
-                                    let attr_name: &str = ::std::stringify!($attr_name);
-                                    let $attr_name = attr_map.get(attr_name)
-                                        .context("Missing attribute, this should never happen")?;
+                if attr_map.values().any(|x| x.is_some()) {
+                    let required_vars: Vec<eww_shared_util::VarName> = attr_map
+                        .values()
+                        .flat_map(|expr| expr.as_ref().map(|x| x.collect_var_refs()).unwrap_or_default())
+                        .collect();
 
-                                    let $attr_name = if let Some(x) = $attr_name {
-                                        Some(x.eval(&values)?.$typecast_func()?)
-                                    } else {
-                                        None
-                                    };
-                                    def_widget!(@unwrap_if_required $attr_name $(? $($optional)?)?);
+                    $args.scope_graph.register_listener(
+                        $args.calling_scope,
+                            crate::state::scope::Listener {
+                            needed_variables: required_vars,
+                            f: Box::new({
+                                let $gtk_widget = $gtk_widget.clone();
+                                move |$scope_graph, values| {
+                                    // value is a map of all the variables that are required to evaluate the
+                                    // attributes expression.
+                                    $(
+                                        let attr_name: &str = ::std::stringify!($attr_name);
+                                        let $attr_name = attr_map.get(attr_name)
+                                            .context("Missing attribute, this should never happen")?;
 
-                                )*
-                                $code
-                                Ok(())
-                            }
-                        }),
-                    },
-                )?;
+                                        let $attr_name = if let Some(x) = $attr_name {
+                                            Some(x.eval(&values)?.$typecast_func()?)
+                                        } else {
+                                            None
+                                        };
+                                        def_widget!(@unwrap_if_required $attr_name $(? $($optional)?)?);
+
+                                    )*
+                                    $code
+                                    Ok(())
+                                }
+                            }),
+                        },
+                    )?;
+                }
             }
         })+
     };
@@ -64,10 +70,11 @@ macro_rules! def_widget {
     };
 
     (@get_value $args:ident, $name:expr, = $default:expr) => {
-        Some($args.widget_use.attrs.ast_optional::<simplexpr::SimplExpr>($name)?.clone().unwrap_or(simplexpr::SimplExpr::synth_literal($default)))
+        Some($args.widget_use.attrs.ast_optional::<simplexpr::SimplExpr>($name)?.clone().unwrap_or_else(|| simplexpr::SimplExpr::synth_literal($default)))
     };
 
     (@get_value $args:ident, $name:expr,) => {
         Some($args.widget_use.attrs.ast_required::<simplexpr::SimplExpr>($name)?.clone())
     }
 }
+
