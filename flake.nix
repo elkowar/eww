be75f052ef878a26ee5e927bb583574308baf7ad
fortuneteller2k <lythe1107@gmail.com>
2022-06-03 23:37:59 +0000

    clean up nix expressions
    tldr
      - update the lockfile, it's months old already
      - remove the old default* attributes
      - refactor some hacks like that pkgs attribute
      - actually make nix flake check happy

--- flake.nix
+++ flake.nix
@@ -1,62 +1,76 @@
 {
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     naersk.url = "github:nmattia/naersk";
   };
   outputs = { self, flake-utils, fenix, nixpkgs, naersk, flake-compat, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
       let
-        # Add rust nightly to pkgs
-        pkgs = nixpkgs.legacyPackages.${system} // { inherit (fenix.packages.${system}.latest) cargo rustc rust-src clippy-preview rustfmt-preview; };
+        pkgs = import nixpkgs { inherit system; };
+        toolchain = fenix.packages.${system}.latest;
 
         naersk-lib = (naersk.lib."${system}".override {
-          cargo = pkgs.cargo;
-          rustc = pkgs.rustc;
+          inherit (toolchain) cargo rustc;
         });
 
-        eww = { wayland ? false }:
+        mkEww = { wayland ? false }:
           naersk-lib.buildPackage {
             pname = "eww";
-            buildInputs = pkgs.lib.optional wayland pkgs.gtk-layer-shell;
+            src = builtins.path { name = "eww"; path = ./.; };
+
             nativeBuildInputs = with pkgs; [ pkg-config gtk3 ];
-            cargoBuildOptions = opts: opts ++ pkgs.lib.optionals wayland [ "--no-default-features" "--features=wayland" ];
-            root = ./.;
+            buildInputs = pkgs.lib.optional wayland pkgs.gtk-layer-shell;
+
+            cargoBuildOptions = opts: opts ++ pkgs.lib.optionals wayland [
+              "--no-default-features"
+              "--features=wayland"
+            ];
+          };
+      in
+      {
+        apps = rec {
+          default = eww;
+          eww = flake-utils.lib.mkApp { drv = self.packages.${system}.eww; };
+          eww-wayland = flake-utils.lib.mkApp { drv = self.packages.${system}.eww-wayland; };
+        };
+
+        packages = rec {
+          default = eww;
+          eww = mkEww { };
+          eww-wayland = mkEww { wayland = true; };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = builtins.attrValues {
+            inherit (toolchain)
+              cargo
+              rustc
+              rust-src
+              clippy-preview
+              rustfmt-preview;
+
+            inherit (pkgs)
+              rust-analyzer
+              gcc
+              gtk3
+              gtk-layer-shell
+              pkg-config
+              deno
+              mdbook;
           };
 
-      in rec {
-        packages.eww = eww {};
-        packages.eww-wayland = eww {wayland=true;};
-
-        defaultPackage = self.packages.${system}.eww;
-
-        apps.eww = flake-utils.lib.mkApp { drv = packages.eww; };
-        apps.eww-wayland = flake-utils.lib.mkApp { drv = packages.eww-wayland; };
-        defaultApp = apps.eww;
-
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            rustc
-            cargo
-            rust-analyzer
-            gcc
-            gtk3
-            gtk-layer-shell
-            pkg-config
-            rustfmt-preview
-            clippy-preview
-            deno
-            mdbook
-          ];
-          RUST_SRC_PATH = "${pkgs.rust-src}/lib/rustlib/src/rust/library";
+          RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
         };
       });
 }
