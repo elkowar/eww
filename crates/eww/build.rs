eba1c032ae3a01ab4c975fcdd9dcf58f0fed8c40
elkowar <5300871+elkowar@users.noreply.github.com>
2022-08-29 13:29:38 +0200

    Add build commit hash to eww --version

---
+++ crates/eww/build.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+fn main() {
+    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output();
+    if let Ok(output) = output {
+        if let Ok(hash) = String::from_utf8(output.stdout) {
+            println!("cargo:rustc-env=GIT_HASH={}", hash);
+            println!("cargo:rustc-env=CARGO_PKG_VERSION={} {}", env!("CARGO_PKG_VERSION"), hash);
+        }
+    }
+    let output = Command::new("git").args(&["show", "-s", "--format=%ci"]).output();
+    if let Ok(output) = output {
+        if let Ok(date) = String::from_utf8(output.stdout) {
+            println!("cargo:rustc-env=GIT_COMMIT_DATE={}", date);
+        }
+    }
+}
