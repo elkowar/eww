2dda57976c2647a413ecfa42e62e6966a1cc6597
fortuneteller2k <lythe1107@gmail.com>
2023-07-06 18:08:07 +0800

    nix: refactor flake
    Things done:
      - removed the overlay, since doing it properly is impossible and requires users to have rust-overlay locally
      - use flake-parts
      - have 3 packages, `eww` for a version with x11 and wayland support, `eww-wayland` for purely wayland, and `eww-x11` for purely x11

--- flake.nix
+++ flake.nix
@@ -1,62 +1,32 @@
 {
   inputs = {
+    # maybe consider __getFlake github:elkowar/eww instead
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    parts.url = "github:hercules-ci/flake-parts";
+    rust.url = "github:oxalica/rust-overlay";
+    nix-filter.url = "github:numtide/nix-filter";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-compat, ... }:
-    let
-      pkgsFor = system: import nixpkgs {
-        inherit system;
+  outputs = inputs@{ self, parts, nixpkgs, rust, ... }:
+    parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-linux" "x86_64-linux" ];
 
-        overlays = [
-          self.overlays.default
-          rust-overlay.overlays.default
-        ];
-      };
+      flake.overlays.default = throw "The eww overlay was removed, please use packages.\${system}.* instead.";
 
-      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
-      mkRustToolchain = pkgs: pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    in
-    {
-      overlays.default = final: prev:
+      perSystem = ctx@{ self', pkgs, lib, system, ... }:
         let
-          rust = mkRustToolchain final;
+          pkgs = ctx.pkgs.extend rust.overlays.default;
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          rustPlatform = prev.makeRustPlatform {
-            cargo = rust;
-            rustc = rust;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
           };
         in
         {
-          eww = (prev.eww.override { inherit rustPlatform; }).overrideAttrs (old: {
-            version = self.rev or "dirty";
-            src = builtins.path { name = "eww"; path = prev.lib.cleanSource ./.; };
-            cargoDeps = rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
-            patches = [ ];
-          });
-
-          eww-wayland = final.eww.override { withWayland = true; };
-        };
-
-      packages = nixpkgs.lib.genAttrs targetSystems (system:
-        let
-          pkgs = pkgsFor system;
-        in
-        (self.overlays.default pkgs pkgs) // {
-          default = self.packages.${system}.eww;
-        }
-      );
-
-      devShells = nixpkgs.lib.genAttrs targetSystems (system:
-        let
-          pkgs = pkgsFor system;
-          rust = mkRustToolchain pkgs;
-        in
-        {
-          default = pkgs.mkShell {
+          devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               rust
               rust-analyzer-unwrapped
@@ -68,9 +38,38 @@
               mdbook
             ];
 
-            RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
           };
-        }
-      );
+
+          packages = {
+            default = self'.packages.eww;
+
+            eww = (pkgs.eww.override { inherit rustPlatform; }).overrideAttrs (old: {
+              version = self.rev or "dirty";
+
+              src = inputs.nix-filter.lib.filter {
+                root = ./.;
+
+                include = [
+                  ./crates
+                  ./Cargo.toml
+                  ./Cargo.lock
+                ];
+              };
+
+              patches = [ ];
+              cargoDeps = rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
+              buildFeatures = [ "wayland" "x11" ];
+              buildInputs = old.buildInputs ++ [ pkgs.gtk-layer-shell ];
+            });
+
+            eww-wayland = self'.packages.eww.overrideAttrs (_: { buildFeatures = [ "wayland" ]; });
+
+            eww-x11 = self'.packages.eww.overrideAttrs (old: {
+              buildFeatures = [ "x11" ];
+              buildInputs = lib.remove pkgs.gtk-layer-shell old.buildInputs;
+            });
+          };
+        };
     };
 }
