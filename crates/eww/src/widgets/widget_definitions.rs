2211e3610bb900fb8a2c35770d95b86ce94fdd39
elkowar <5300871+elkowar@users.noreply.github.com>
2024-02-17 12:43:14 +0100

    Yeet hash_extract_if, let_chains

--- crates/eww/src/widgets/widget_definitions.rs
+++ crates/eww/src/widgets/widget_definitions.rs
@@ -128,8 +128,7 @@ static DEPRECATED_ATTRS: Lazy<HashSet<&str>> =
 /// @widget widget
 /// @desc these properties apply to _all_ widgets, and can be used anywhere!
 pub(super) fn resolve_widget_attrs(bargs: &mut BuilderArgs, gtk_widget: &gtk::Widget) -> Result<()> {
-    let deprecated: HashSet<_> = DEPRECATED_ATTRS.to_owned();
-    let contained_deprecated: Vec<_> = bargs.unhandled_attrs.extract_if(|a, _| deprecated.contains(&a.0 as &str)).collect();
+    let contained_deprecated: Vec<_> = DEPRECATED_ATTRS.iter().filter_map(|x| bargs.unhandled_attrs.remove_entry(*x)).collect();
     if !contained_deprecated.is_empty() {
         let diag = error_handling_ctx::stringify_diagnostic(gen_diagnostic! {
             kind =  Severity::Error,
