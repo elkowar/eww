b17924079428c2e2bc36b1ce851bf17ff7c3a88a
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-08-24 13:48:08 +0200

    fix: fix lint
    i introduced this in #1119, whoops

--- crates/eww/src/widgets/widget_definitions.rs
+++ crates/eww/src/widgets/widget_definitions.rs
@@ -1147,7 +1147,7 @@ const WIDGET_NAME_STACK: &str = "stack";
 fn build_gtk_stack(bargs: &mut BuilderArgs) -> Result<gtk::Stack> {
     let gtk_widget = gtk::Stack::new();
 
-    if bargs.widget_use.children.len() < 1 {
+    if bargs.widget_use.children.is_empty() {
         return Err(DiagError(gen_diagnostic!("stack must contain at least one element", bargs.widget_use.span)).into());
     }
 
