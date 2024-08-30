bc5e1fc64c3d54d76f0ac6f54fdc96cd6ddd898d
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-08-30 12:15:19 +0200

    docs: document changes to autogenerated files

--- crates/notifier_host/src/proxy/mod.rs
+++ crates/notifier_host/src/proxy/mod.rs
@@ -5,6 +5,11 @@
 //! generated with [zbus-xmlgen](https://docs.rs/crate/zbus_xmlgen/latest) by running
 //! `zbus-xmlgen file crates/notifier_host/src/proxy/dbus_status_notifier_item.xml` and
 //! `zbus-xmlgen file crates/notifier_host/src/proxy/dbus_status_notifier_watcher.xml`.
+//!
+//! Note that the `dbus_status_notifier_watcher.rs` file has been slightly adjusted, the
+//! default arguments to the [proxy](https://docs.rs/zbus/4.4.0/zbus/attr.proxy.html)
+//! macro need some adjusting.
+//!
 //! At the moment, `dbus_menu.xml` isn't used.
 //!
 //! For more information, see ["Writing a client proxy" in the zbus
