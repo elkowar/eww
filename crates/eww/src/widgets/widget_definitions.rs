59f4dbd43bfbd370392f31f9ec76731f2d428dcf
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-09-07 13:57:36 +0200

    chore: switch to new unescaping crate

--- crates/eww/src/widgets/widget_definitions.rs
+++ crates/eww/src/widgets/widget_definitions.rs
@@ -992,7 +992,7 @@ fn build_gtk_label(bargs: &mut BuilderArgs) -> Result<gtk::Label> {
                 }
             };
 
-            let text = unescape::unescape(&text).context(format!("Failed to unescape label text {}", &text))?;
+            let text = unescaper::unescape(&text)?;
             let text = if unindent { util::unindent(&text) } else { text };
             gtk_widget.set_text(&text);
         },
