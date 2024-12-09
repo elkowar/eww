dd09932f3cf4fa92ec2bdd31d5dc76ffc357c79e
Fejiberglibstein <amthein321@gmail.com>
2024-12-08 23:07:14 -0500

    Handling Documents

--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::build(|client| Backend { client }).finish();
+    let (service, socket) = LspService::build(|client| Backend::new(client)).finish();
 
     Server::new(stdin, stdout, socket).serve(service).await;
 }
