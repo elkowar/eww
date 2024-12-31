bd66241b2b0fb8d954a63d4276db51ec9071c401
Mirko Hahn <4210075+mirhahn@users.noreply.github.com>
2024-12-31 14:56:38 +0100

    fix: get_locale follows posix standard

--- crates/eww_shared_util/src/locale.rs
+++ crates/eww_shared_util/src/locale.rs
@@ -1,11 +1,12 @@
 use chrono::Locale;
 use std::env::var;
 
-/// Returns the `Locale` enum based on the `LC_TIME` environment variable.
+/// Returns the `Locale` enum based on the `LC_ALL`, `LC_TIME`, and `LANG` environment variables in
+/// that order, which is the precedence order prescribed by Section 8.2 of POSIX.1-2008.
 /// If the environment variable is not defined or is malformed use the POSIX locale.
 pub fn get_locale() -> Locale {
-    let locale_string: String =
-        var("LC_TIME").map_or_else(|_| "C".to_string(), |v| v.split(".").next().unwrap_or("C").to_string());
+    let locale_env = var("LC_ALL").or_else(|_| var("LC_TIME").or_else(|_| var("LANG")));
+    let locale_string: String = locale_env.map_or_else(|_| "C".to_string(), |v| v.split(".").next().unwrap_or("C").to_string());
 
     match (&*locale_string).try_into() {
         Ok(x) => x,
